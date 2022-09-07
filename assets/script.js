158171ea8f4f86a3ecf672d1336d4b0ac7278dd3
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-09-07 09:17:34 -0500

    morning save

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,19 @@
-var city = "new york"
+var city = document.querySelector("#savecity")
 var appid = "c827936063ee461a4cc089216c08d956"
+var search = document.querySelector("button")
 
-var apiurl = 'https://api.openweathermap.org/data/2.5/forecast?q='+ city +'&appid='+ appid +'';
+  search.addEventListener('click', function(){
+    var searchsave = city.value; 
+    localStorage.setItem("city", searchsave)
+  });
 
-fetch(apiurl)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+  var citysave = localStorage.getItem("city")
 
-  console.log(apiurl)
+  console.log(citysave)
+
+  var apiurl = 'https://api.openweathermap.org/data/2.5/forecast?q='+ citysave +'&appid='+ appid +'';
+
+  fetch(apiurl)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  
