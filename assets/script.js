c45a43c21db957065cf78ae8bf6df786a10c8a92
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-09-11 18:33:46 -0500

    added uv colors to display uv safety

--- assets/script.js
+++ assets/script.js
@@ -5,32 +5,32 @@ var search = document.querySelector("button")
 var temp = document.querySelector("#temp")
 var wind = document.querySelector("#wind")
 var humidity = document.querySelector("#humidity")
-var uv = document.querySelector("#uv")
+var uvtext = document.querySelector("#uv")
 
 var temp1 = document.querySelector("#temp1")
 var wind1 = document.querySelector("#wind1")
 var humidity1 = document.querySelector("#humidity1")
-var uv1 = document.querySelector("#uv1")
+var uvtext1 = document.querySelector("#uv1")
 
 var temp2 = document.querySelector("#temp2")
 var wind2 = document.querySelector("#wind2")
 var humidity2 = document.querySelector("#humidity2")
-var uv2 = document.querySelector("#uv1")
+var uvtext2 = document.querySelector("#uv2")
 
 var temp3 = document.querySelector("#temp3")
 var wind3 = document.querySelector("#wind3")
 var humidity3 = document.querySelector("#humidity3")
-var uv3 = document.querySelector("#uv3")
+var uvtext3 = document.querySelector("#uv3")
 
 var temp4 = document.querySelector("#temp4")
 var wind4 = document.querySelector("#wind4")
 var humidity4 = document.querySelector("#humidity4")
-var uv4 = document.querySelector("#uv4")
+var uvtext4 = document.querySelector("#uv4")
 
 var temp5 = document.querySelector("#temp5")
 var wind5 = document.querySelector("#wind5")
 var humidity5 = document.querySelector("#humidity5")
-var uv5 = document.querySelector("#uv5")
+var uvtext5 = document.querySelector("#uv5")
 
 var today = document.querySelector("#today")
 var day1 = document.querySelector("#day1")
@@ -80,7 +80,60 @@ var day5 = document.querySelector("#day5")
       humidity3.textContent = weather.list[3].main.humidity;
       humidity4.textContent = weather.list[4].main.humidity;
       humidity5.textContent = weather.list[5].main.humidity;
-    });
+
+      var lat = weather.city.coord.lat
+      var lon = weather.city.coord.lon
+
+      console.log(lat)
+      console.log(lon)
+
+      var uvapi= 'https://api.openweathermap.org/data/2.5/uvi?appid='+ appid +'&lat='+ lat +'&lon='+ lon +'';
+
+      fetch(uvapi)
+      .then((response) => response.json())
+      .then((data)=> {
+        var uv = data
+        var uvval = uv.value
+        console.log(uvval)
+        if (uvval >= 1, uvval <= 2) {
+          uvtext.style.color = "green";
+          uvtext1.style.color = "green";
+          uvtext2.style.color = "green";
+          uvtext3.style.color = "green";
+          uvtext4.style.color = "green";
+          uvtext5.style.color = "green";
+
+        } else if (uvval >= 3, uvval <= 5) {
+          uvtext.style.color = "yellow";
+          uvtext1.style.color = "yellow";
+          uvtext2.style.color = "yellow";
+          uvtext3.style.color = "yellow";
+          uvtext4.style.color = "yellow";
+          uvtext5.style.color = "yellow";  
+        
+        } else if (uvval >= 6, uvval <= 7 ){
+          uvtext.style.color = "orange";
+          uvtext1.style.color = "orange";
+          uvtext2.style.color = "orange";
+          uvtext3.style.color = "orange";
+          uvtext4.style.color = "orange";
+          uvtext5.style.color = "orange";
+
+        } else if (uvval >= 8, uvval <= 10) {
+          uvtext.style.color = "red";
+          uvtext1.style.color = "red";
+          uvtext2.style.color = "red";
+          uvtext3.style.color = "red";
+          uvtext4.style.color = "red";
+          uvtext5.style.color = "red";
+
+        } 
+
+        }
+    )});
+    
+
+
 
     today.textContent = moment().calendar()
     day1.textContent = moment().add(1, 'days').calendar()
